5c0578944e7388c51287f44eed079f01e3221cca
haruue <57939160+haruue1@users.noreply.github.com>
2023-04-28 22:03:50 -0400

    add ghost boi enemy
    ghost boi enemy now chases ur dumbass around until you kill it

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#game-layer")
+const deadboi = document.querySelector(".boi-is-dead")
 
 /** @type {CanvasRenderingContext2D} */
 const c = canvas.getContext("2d")
@@ -6,8 +7,10 @@ const c = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 let projectiles = []
+let ghosts = []
 let isShooting = false
 let mouse = {x: 0, y: 0}
+let score = 0
 
 const keys = {
     w: false,
@@ -24,6 +27,7 @@ class Entity {
         this.radius = radius
         this.color = color
         this.speed = 1 
+        this.dead = false
     }
 
     update(delta) {
@@ -39,14 +43,44 @@ class Entity {
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false)
         c.fillStyle = this.color
         c.fill()
+        c.closePath()
+    }
+}
+
+class Ghost {
+    constructor(position, direction, health = 100) {
+        this.image = new Image()
+        this.position = position
+        this.direction = direction
+        this.velocity = {x: 0, y: 0}
+        this.health = health
+    }
+
+    update(delta) {
+        let dx = player.position.x - this.position.x
+        let dy = player.position.y - this.position.y
+        let angle = Math.atan2(dy, dx)
+        this.velocity.x = Math.cos(angle)
+        this.velocity.y = Math.sin(angle)
+
+        this.position.x += this.velocity.x 
+        this.position.y += this.velocity.y 
+    }
+
+    draw() {
+        this.image.src = "assets/ghost.png"
+        let x = this.position.x - this.image.width / 2;
+        let y = this.position.y - this.image.height / 2;
+        c.drawImage(this.image, x, y)
     }
 }
 
 class Projectile extends Entity {
     static fireRate = 50 
-    constructor(position, direction, radius, color, projectileSpeed) {
+    constructor(position, direction, radius, color, projectileSpeed = 1, projectileDamage = 5) {
         super(position, direction, radius, color)
         this.projectileSpeed = projectileSpeed
+        this.projectileDamage = projectileDamage
     }
 
     fire() {
@@ -64,9 +98,17 @@ class Projectile extends Entity {
 }
 
 function fireProjectile() {
-    let newProjectile = new Projectile({x: player.position.x, y: player.position.y}, {x: mouse.x, y: mouse.y}, 5, "#ff0000", 1)
+    let newProjectile = new Projectile({x: player.position.x, y: player.position.y}, {x: mouse.x, y: mouse.y}, 5, "#ff0000")
     newProjectile.fire()
     projectiles.push(newProjectile)
+}
+
+function checkCollision(entity1, entity2) {
+    if (entity2.position.x > entity1.position.x - (75/2) && entity2.position.x < entity1.position.x + (75/2) &&
+        entity2.position.y > entity1.position.y - (75/2) && entity2.position.y < entity1.position.y + (75/2)) {
+            return true
+        }
+    return false
 }
 
 const player = new Entity(
@@ -75,6 +117,14 @@ const player = new Entity(
     10,
     "#88c0d0",
 )
+
+function spawnGhost() {
+    let x = Math.random() * canvas.width
+    let y = 100
+
+    ghosts.push(new Ghost({x: x, y: y}, {x: 0, y: 0}, 50))
+}
+
 
 addEventListener("keydown", e => {
     keys[e.key] = true
@@ -102,12 +152,28 @@ let last_fire = 0
 let fps = 0
 
 function draw_fps(value) {
-    c.font = "30px Arial"
-    c.fillText(value, 10, 40)
+    c.font = "10px Arial"
+    c.fillStyle = "#a3be8c"
+    c.fillText(value, 10, 20)
+}
+
+function draw_score() {
+    const size = 50
+    c.font = `${size}px Arial`
+    c.fillStyle = "#a3be8c"
+    c.fillText("Score: " + score, 10, size + 20)
+}
+
+function update_score(value) {
+    score += value
+}
+
+function clear_screen() {
+    c.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 function game_loop(timestamp) {
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    clear_screen()
     let delta = timestamp - last_timestamp
 
     if (keys.a) {
@@ -133,26 +199,57 @@ function game_loop(timestamp) {
         }
     }
     
+    player.update(delta)
+    player.draw()
+    
+    ghosts.forEach(ghost => {
+        ghost.update(delta)
+        ghost.draw()
+        if (checkCollision(ghost, player)) {
+            player.dead = true
+        }
+    })
+
     projectiles.forEach((projectile, index) => {
         if (projectile.position.x > 0 && projectile.position.x < canvas.width && 
             projectile.position.y > 0 && projectile.position.y < canvas.height) {
             projectile.update(delta)
             projectile.draw()
+            ghosts.forEach((ghost, ghostIndex) => {
+
+                if (checkCollision(ghost, projectile)) {
+                    projectiles.splice(index, 1)
+                    ghost.health -= projectile.projectileDamage
+                }
+                
+                if (ghost.health <= 0) {
+                    ghosts.splice(ghostIndex, 1)
+                    update_score(100)
+                }
+            })
         } else {
             projectiles.splice(index, 1)
         }
     })
 
 
-    player.update(delta)
-    player.draw()
     
     fps = 1000 / delta
     draw_fps("FPS: " + Math.round(fps))
+    draw_score()
     
     last_timestamp = timestamp
-    requestAnimationFrame(game_loop)
+
+    if (!player.dead) {
+        requestAnimationFrame(game_loop)
+    } else {
+        clearInterval(ghostSpawner)
+        clear_screen()
+        canvas.style.display = "none"
+        deadboi.style.display = "block"
+    }
 }
 requestAnimationFrame(game_loop)
+const ghostSpawner = setInterval(spawnGhost, 1000)
 
 
